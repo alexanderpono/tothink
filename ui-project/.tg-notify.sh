4468553492efb8adaeb637dc7f031b8e12af93db
Alexander Ponomarenko <brain16383@yandex.ru>
2021-11-27 21:37:18 +0300

    [gha-test] CI-15 added generation of test report

--- ui-project/.tg-notify.sh
+++ ui-project/.tg-notify.sh
@@ -8,7 +8,7 @@ echo "MSG=$MSG"
 # TEXT="Тестирование выполнено: (☉_☉)"
 curl -s -X POST https://api.telegram.org/$TELEGRAM_BOT_TOKEN/sendMessage -d chat_id=$TELEGRAM_USER_ID  -d text="$MSG"
 
-# curl -F chat_id=$TELEGRAM_USER_ID -F document=@"./temp/testResult.html" https://api.telegram.org/$TELEGRAM_BOT_TOKEN/sendDocument
+curl -F chat_id=$TELEGRAM_USER_ID -F document=@"./temp/testResult.html" https://api.telegram.org/$TELEGRAM_BOT_TOKEN/sendDocument
 
 
 #%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG%0A
--- ui-project/jest.config.js
+++ ui-project/jest.config.js
@@ -5,13 +5,28 @@ module.exports = {
     clearMocks: true,
     coverageDirectory: 'coverage',
     collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.(ts|tsx)', '!src/**/*.stories.tsx', '!src/app.tsx', '!src/**/index.tsx'],
+    collectCoverageFrom: [
+        'src/**/*.(ts|tsx)',
+        '!src/**/*.stories.tsx',
+        '!src/app.tsx',
+        '!src/**/index.tsx',
+    ],
     testEnvironment: 'jsdom',
+    reporters: [
+        'default',
+        [
+            './node_modules/jest-html-reporter',
+            {
+                pageTitle: 'Test Report',
+                outputPath: './temp/testResult.html',
+            },
+        ],
+    ],
     setupFilesAfterEnv: ['<rootDir>/internals/jestSettings.js'],
     transform: {
-        '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
+        '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
     },
     moduleNameMapper: {
-        '\\.(css|less)$': '<rootDir>/internals/__mocks__/styleMock.js'
-    }
+        '\\.(css|less)$': '<rootDir>/internals/__mocks__/styleMock.js',
+    },
 };
