507b47556517d39810747ee45ab1a4cbf882104c
Alexander Ponomarenko <brain16383@yandex.ru>
2021-11-27 22:09:22 +0300

    [gha-test] CI-17 fix report link at TG message

--- ui-project/.tg-notify.sh
+++ ui-project/.tg-notify.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-# var="hello"
-# echo "$var world"
-# echo "TEST_VAL1=$TEST_VAL1"
-# echo "TEST_VAL2=$TEST_VAL2"
 echo "MSG=$MSG"
 
-# TEXT="Тестирование выполнено: (☉_☉)"
 curl -s -X POST https://api.telegram.org/$TELEGRAM_BOT_TOKEN/sendMessage -d chat_id=$TELEGRAM_USER_ID  -d text="$MSG"
 
-curl -F chat_id=$TELEGRAM_USER_ID -F document=@"./temp/testResult.html" https://api.telegram.org/$TELEGRAM_BOT_TOKEN/sendDocument
+# curl -F chat_id=$TELEGRAM_USER_ID -F document=@"./temp/testResult.html" https://api.telegram.org/$TELEGRAM_BOT_TOKEN/sendDocument
 
-
-#%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG%0A
-#отчет доступен по ссылке: https://gitlab.com/alexanderpono/otus-qa-js-hw-10/-/jobs/$CI_JOB_ID/artifacts/public/testResult.html
-#%0A краткий отчет:
