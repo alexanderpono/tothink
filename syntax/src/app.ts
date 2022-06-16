066bf1d727cc225bf6fae266b3b4f0a19ad8ce70
Alexander Ponomarenko <brain16383@yandex.ru>
2022-06-17 02:21:17 +0300

    [syntax] added nodemon

--- syntax/src/app.ts
+++ syntax/src/app.ts
@@ -1,28 +1,38 @@
-import { createSpecForReducer, lexReducerSpec } from './cards';
+import {
+    createReducerSpec,
+    lexReducerSpecModel,
+    createReducerSpec2,
+    lexReducerSpecModel2
+} from './cards';
 import { defaultLexState, lex, lexReducer } from './lexReducer';
 import { defaultTextState, textReducer, TextState } from './textReducer';
 
-// let state = { ...defaultLexState };
-// const text = `
-// function a(b) {
-//     return b;
-// }
-// `;
-// const actions = [
-//     lex.spaces([' ']),
-//     lex.limiters([';', '(', ')', '{', '}']),
-//     lex.compile(),
-//     lex.parseText(text)
-// ];
+let state0 = { ...defaultLexState };
 
-// actions.forEach((action) => {
-//     state = lexReducer(state, action);
-// });
+[
+    lex.spaces([' ']),
+    lex.limiters([';', '(', ')', '{', '}']),
+    lex.compile(),
+    lex.parseText(`
+    function a(b) {
+        return b;
+    }
+    `)
+].forEach((action) => {
+    state0 = lexReducer(state0, action);
+});
 
 // console.log('state=', state);
 
-const state: TextState = createSpecForReducer(lexReducerSpec).reduce(
+const state: TextState = createReducerSpec(lexReducerSpecModel).reduce(
     (state: TextState, action) => textReducer(state, action),
     { ...defaultTextState }
 );
-console.log(state.canvases.result);
+// console.log('=============== createLexReducerSpec ===============');
+// console.log(state.canvases.result);
+
+const state2: TextState = createReducerSpec2(lexReducerSpecModel2).reduce(
+    (state: TextState, action) => textReducer(state, action),
+    { ...defaultTextState }
+);
+console.log(state2.canvases.result);
