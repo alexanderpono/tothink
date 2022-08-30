77b41632414aff90fd15b90d50c77052fac40073
Alexander Ponomarenko <brain16383@yandex.ru>
2022-08-31 01:43:27 +0300

    [webpack-dynamic-imports] - import with async/await syntax

--- webpack-async-import/dynamic-imports/src/index.js
+++ webpack-async-import/dynamic-imports/src/index.js
@@ -1,12 +1,8 @@
-function getComponent() {
-    return import('lodash')
-        .then(({ default: _ }) => {
-            const element = document.createElement('div');
-
-            element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-            return element;
-        })
-        .catch((error) => 'An error occurred while loading the component');
+async function getComponent() {
+    const element = document.createElement('div');
+    const { default: _ } = await import('lodash');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return element;
 }
 
 getComponent().then((component) => {
