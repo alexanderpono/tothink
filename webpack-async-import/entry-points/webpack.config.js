095a8cb79c4a66603aad9abaadd342f8d77acfbb
Alexander Ponomarenko <brain16383@yandex.ru>
2022-08-31 01:17:52 +0300

    [webpack-entry-points] deduplication - entry dependecies

--- webpack-async-import/entry-points/webpack.config.js
+++ webpack-async-import/entry-points/webpack.config.js
@@ -3,11 +3,21 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: {
-        index: './src/index.js',
-        another: './src/another-module.js',
+        index: {
+            import: './src/index.js',
+            dependOn: 'shared',
+        },
+        another: {
+            import: './src/another-module.js',
+            dependOn: 'shared',
+        },
+        shared: 'lodash',
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+        runtimeChunk: 'single',
     },
 };
