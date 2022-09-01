3d47aecdea883cb83c8fff61ac849d4a86fe8dfe
alexanderpono <brain16383@yandex.ru>
2022-09-01 22:30:20 +0300

    [app2d] add PIXI.js

--- app2d/src/Application.js
+++ app2d/src/Application.js
@@ -1,41 +1,43 @@
 import logoImg from "../assets/logo.svg";
+import * as PIXI from 'pixi.js';
 
 export class Application {
     constructor() {
-        this.view = document.createElement("canvas");
-        this.view.width = 640;
-        this.view.height = 480;
+        this.renderer = new PIXI.Renderer({
+            width: 640,
+            height: 480,
+            backgroundColor: 0xffc0cb
+        })
+        this.root = new PIXI.Container();
 
         this.image = new Image();
         this.image.src = logoImg;
+    }
 
-        this.context = this.view.getContext("2d");
+    get view() {
+        return this.renderer.view;
     }
 
     start() {
         this.phase = 0;
+
+        const { width, height } = this.view;
+        this.sprite = new PIXI.Sprite(PIXI.Texture.from(this.image))
+        this.sprite.position.set(width/2, height/2);
+        this.sprite.scale.set(0.9, 0.9);
+        this.sprite.anchor.set(0.5, 0.5);
+        this.root.addChild(this.sprite);
+
         const gameLoop = () => {
-          this.phase += 0.01;
-          this.render();
-          requestAnimationFrame(gameLoop);
+            this.phase += 0.01;
+            this.sprite.rotation = this.phase;
+            this.render();
+            requestAnimationFrame(gameLoop);
         };
         requestAnimationFrame(gameLoop);
-      }    
+    }
 
     render() {
-        const { width, height } = this.view;
-        this.context.fillStyle = "blue";
-        this.context.fillRect(0, 0, width, height);
-
-        const imageWidth = 248;
-        const imageHeight = 192;
-    
-        this.context.save();
-        this.context.translate(width / 2, height / 2);
-        this.context.scale(0.9, 0.9);
-        this.context.rotate(this.phase);
-    
-        this.context.drawImage(this.image, -imageWidth / 2, -imageHeight / 2);
-        this.context.restore();
+        this.renderer.render(this.root);
     }
 }
