fe6e628ab989164a6b5cbdc7e668b27145531da3
Alexander Ponomarenko <brain16383@yandex.ru>
2022-09-02 15:14:47 +0300

    [my-hw-cli] inspirated by AKorzhikov

--- my-hw-cli/index.js
+++ my-hw-cli/index.js
@@ -1,31 +1,24 @@
 #!/usr/bin/env node
+const { description, name, version } = require('./package.json');
 
-const package_json = require('./package.json');
-
-let processed = false;
-if (process.argv.findIndex((item) => item === '--version') >= 0) {
-    console.log('version ' + package_json.version);
-    processed = true;
-}
-
-if (process.argv.findIndex((item) => item === '--help') >= 0) {
-    console.log(`
-mw-hw-cli
-
-Package version
-${package_json.version}
-
-Package description
-${package_json.description}
+const options = process.argv.slice(2);
+const VERSION_MESSAGE = `${name} ${version}`;
+const HELP_MESSAGE = `${VERSION_MESSAGE}
+${description}
 
 Usage
 --help    Help documentation
 --version Installed package version
-    `);
-    processed = true;
+
+`;
+
+if (options.includes('--version')) {
+    console.log(VERSION_MESSAGE);
+    return;
 }
 
-if (processed) {
+if (options.includes('--help')) {
+    console.log(HELP_MESSAGE);
     return;
 }
 
