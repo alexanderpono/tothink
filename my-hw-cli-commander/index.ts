667e74d0119f228a1658e018ef47bd6fbccb7584
Alexander Ponomarenko <brain16383@yandex.ru>
2022-09-03 01:54:34 +0300

    [my-hw-cli] commander: added CLI

--- my-hw-cli-commander/index.ts
+++ my-hw-cli-commander/index.ts
@@ -1,22 +1,40 @@
 #!/usr/bin/env ts-node
 const { description, name, version } = require('./package.json');
+import { program } from 'commander';
 
-const options = process.argv.slice(2);
-const VERSION_MESSAGE = `${name} ${version}`;
-const HELP_MESSAGE = `${VERSION_MESSAGE}
-${description}
+program
+    .name(name)
+    .version(version)
+    .description(description)
+    .option('-p, --peppers', 'Add peppers')
+    .option('-P, --pineapple', 'Add pineapple')
+    .option('-b, --bbq-sauce', 'Add bbq sauce')
+    .option('-c, --cheese [type]', 'Add cheese with optional type')
+    .parse(process.argv);
 
-Usage
---help    Help documentation
---version Installed package version
+const options = program.opts();
+if (Object.keys(options).length === 0) {
+    program.help();
+}
 
-`;
 
-if (options.includes('--version')) {
-    console.log(VERSION_MESSAGE);
-    process.exit(0);
-} else {
-    console.log(HELP_MESSAGE);
-    process.exit(0);
+console.log('You ordered a pizza with ');
+if (options.peppers) {
+    console.log(' - peppers');
+}
+if (options.pineapple) {
+    console.log(' - pineapple');
+}
+if (options.bbqSauce) {
+    console.log(' - bbq sauce');
+}
+
+if (options.cheese === undefined) {
+    console.log('no cheese');
+} else if (options.cheese === true) {
+    console.log('add cheese');
+}
+else {
+    console.log(`add cheese type ${options.cheese}`);
 }
 
